8eb75729b6e5ab4d87e450ad61ac8b79912d9b5f
Adam Kalt <adam.kalt.1@gmail.com>
2018-03-18 22:46:43 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ require "pry"
 def turn_count(board)
   turns = 0
 board.each do |position|
-  if position == "X" || position = "O"
+  if position == "X" || position == "O"
     turns +=1
   end
 end
